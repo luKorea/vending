2b480588655f3f02a452378d36912e1aa5c5ff81
dengpeng <78168362@qq.com>
2020-08-05 15:48:49 +0800

    u

--- vending-v2/config/webpack.dev.conf.js
+++ vending-v2/config/webpack.dev.conf.js
@@ -13,13 +13,13 @@ module.exports = merge(base, {
         port: "8075", //设置默认监听端口，如果省略，默认为"8080"
         inline: false, //实时刷新
         historyApiFallback: true, //不跳转
-        host: '172.16.68.254',
+        // host: '172.16.68.254',
         //代理转发接口
         proxy: {
             //把/api/t转发到target，但是转发的是http://xxx/api/t
             //不要/api,用pathRewrite
             '/api': {
-                target: 'http://172.16.71.142:8086', //(跨域的地址)
+                target: 'http://127.0.0.1:8086', //(跨域的地址)
                 changeOrigin: true,
                 pathRewrite: {
                     '^/api': ''
