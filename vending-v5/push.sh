a2c0cd7703842e04dfc1f78cf0a5857383c6d8c0
korealu <643949593@qq.com>
2021-04-06 10:58:32 +0800

    korea 2021-04-06 10:58:32

--- vending-v5/push.sh
+++ vending-v5/push.sh
@@ -3,7 +3,7 @@ basepath=$(cd `dirname $0`; pwd)
 cd  $basepath
 git add .
 remark=$(date +"%Y-%m-%d %H:%M:%S")
-git commit -m "korea：${remark}"
+git commit -m "korea ${remark}"
 # git pull origin dev
 git push origin dev
 echo "---END---"
