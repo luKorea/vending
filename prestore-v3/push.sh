43068df1794b2c706b4d74fefec4a2d86745f0b7
korealu <643949593@qq.com>
2021-04-08 16:38:36 +0800

    korea 2021-04-08 16:38:36

---
+++ prestore-v3/push.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+baseball=$(
+  cd $(dirname $0) || exit
+  pwd
+)
+cd "$baseball" || exit
+npm run build
+git add .
+remark=$(date +"%Y-%m-%d %H:%M:%S")
+git commit -m "korea ${remark}"
+# git pull origin dev
+git push origin dev
+echo -e "\033[31m ---The Project Success Build And Push--- \033[0m"
