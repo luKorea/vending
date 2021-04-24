ebf1a6ec0b1c09aae6ede060ca6957a54e23b0f0
dengpeng <78168362@qq.com>
2021-04-24 16:58:50 +0800

    u

--- vending-v1/js/public/documentWrite.js
+++
@@ -1,6 +0,0 @@
-document.write(`<script src="../../js/public/jquery-3.1.1.min.js"></script>`);
-document.write(`<link rel="stylesheet" href="../../MyCss/public/pubilc.css">`);
-document.write(` <link rel="stylesheet" href="../../layui/css/layui.css">`);
-document.write(`<script src="../../js/public/api.js"></script>`);
-document.write(`<script src="../../layui/layui.js"></script>`);
-document.write(`<script src="../../js/public/mian.js"></script>`);
